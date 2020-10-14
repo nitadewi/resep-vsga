4de3e53ad2b4559505e069ff9ba151ea7343a7b0
nita dewi <nitadewis@gmail.com>
2020-10-14 16:14:35 +0700

    nambah form register dan folder layout res untuk menyimpan layout login dan register

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php include 'layoutRes/header.php'; ?>
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="p-5" style="width:100%">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                  </div>
+                  <form class="user">
+                  <div class="form-group">
+                      <input type="text" class="form-control form-control-user"   placeholder="Enter Name..">
+                    </div>
+                    <div class="form-group">
+                      <input type="text" class="form-control form-control-user"   placeholder="Username..">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user"  placeholder="Password">
+                    </div>
+                    
+                    <a href="login.html" class="btn btn-primary btn-user btn-block">
+                  Register Account
+                </a>
+                  
+                </div>
+              
+            </div>
+
+<?php include 'layoutRes/footer.php'; ?>           
