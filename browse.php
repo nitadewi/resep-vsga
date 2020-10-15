9fa08510eea84280ff99218278e0b019372e1340
nita dewi <nitadewis@gmail.com>
2020-10-15 13:00:57 +0700

    pop up resep

--- browse.php
+++ browse.php
@@ -9,9 +9,14 @@ $db = new Database();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resep Harian</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core JavaScript-->
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -46,66 +51,63 @@ $db = new Database();
 
 
     <div class="container">
+
+        <!--lihat postingan-->
+        <div class="modal fade" id="get-data" tabindex="-1" role="dialog" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Resep</h5>
+
+              </div>
+              <div class="modal-body"></div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-pink" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
     
                   
-      <div class="row">
-      <?php
-                            $no = 1;
-                            foreach ($db->show() as $data) { ?>
-          <div class="col-4">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="Gambar">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $data['judul']; ?></h5>
-                    <a href='javascript:void(0)' class="btn btn-pink get_id" data-id="<?= $data['id'] ?>" data-toggle="modal" data-target="#myModal">Lihat</a>
-                    <!-- <button type="button" class="btn-pink" data-toggle="modal" data-target="#exampleModal">Lihat</button> -->
-                   
+          <div class="row">
+            <?php
+                                  $no = 1;
+                                  foreach ($db->show() as $data) { ?>
+                <div class="col-4">
+                  <div class="card">
+                      <img src="..." class="card-img-top" alt="Gambar">
+                      <div class="card-body">
+                          <h5 class="card-title"><?= $data['judul']; ?></h5>
+                          <a href="#"class="open-modal btn btn-pink" data-id="<?= $data['id']; ?>" data-toggle="modal">Lihat</a>
+                        
+                      </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-        <?php	
-                  }
-                  ?>
-      </div>
+            <?php	} ?>
+          </div>
     
      
     </div>
+   
 
-    <!--lihat postingan-->
-
-    <div id="myModal" class="modal fade" role="dialog">
-         <div class="modal-dialog">
-            <!-- Modal content-->
-            <div class="modal-content">
-               
-               <div class="modal-body" id="load_data">
-               </div>
-               <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <script>
+      <script type="text/javascript">
         $(function() {
-          $(document.on('click','.open-modal',function(e){
+          $(document).on('click','.open-modal',function(e){
             e.preventDefault();
             $("#get-data").modal('show');
             $.post('lihat.php',{id:$(this).attr('data-id')},
             function(html){
-              $(".modal-body")
-            } )
-          } ))
+              $(".modal-body").html(html);
+            });
           });
+        });
           
 
 	  
 	  </script>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
