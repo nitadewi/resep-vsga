c60649fab9b5b659e9b702667e4483cfc9cbcf53
Rahmad Susilo <rahmadgnr78@gmail.com>
2020-10-15 20:12:17 +0700

    git

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
 
 
 
-   
+
     <link rel="stylesheet" href="css/style.css">
 
     <script>
-    $(function() {
-        $("#cari").autocomplete({
-            source: 'core/cari.php'
-         });
-    });
-</script>
+        $(function() {
+            $("#cari").autocomplete({
+                source: 'core/cari.php'
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -32,7 +32,7 @@
         <div class="container">
             <a class="navbar-brand" href="#">Logo</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -46,6 +46,9 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="browse.php">Browse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="register.php">Register</a>
@@ -82,7 +85,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-   
+
 </body>
 
 </html>
